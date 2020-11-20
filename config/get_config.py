8131bbd209a780ffc37646969b4e9b5898082756
Quinten <tintin10q@hotmail.com>
2020-11-20 19:10:19 +0100

    Added path to config file 🚓

--- config/get_config.py
+++ config/get_config.py
@@ -1,4 +1,8 @@
 import json
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def get_config():
-    return json.load(open("config.json"))
+
+    return json.load(open(dir_path+"\\config.json"))
