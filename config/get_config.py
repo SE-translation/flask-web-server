9a14cc09eb56b8c34447ead296079d4f74c4d564
Quinten <tintin10q@hotmail.com>
2020-11-20 19:53:11 +0100

    Changed \\ into / in file path 😒

--- config/get_config.py
+++ config/get_config.py
@@ -4,5 +4,4 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def get_config():
-
-    return json.load(open(dir_path+"\\config.json"))
+    return json.load(open(dir_path+"/config.json"))
