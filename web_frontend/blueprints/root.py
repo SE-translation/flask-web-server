f2e1b7e3cfa33254335b4b670cf841f9f1adf890
Ubuntu <ubuntu@ip-172-31-16-159.us-east-2.compute.internal>
2020-11-26 22:04:30 +0000

    trying to get model type

--- web_frontend/blueprints/root.py
+++ web_frontend/blueprints/root.py
@@ -17,9 +17,10 @@ def root_get():
     return render_template("root.html")
 
 
+model_type = {"small":100, "big":101}
+
 @root_blueprint.route("/", methods=["POST"])
 def root_post():
-    
     file = request.files['audio_file']
     if not secure_filename(file.filename):
         return render_template("root.html", text="Something was wrong with the filename")
@@ -30,6 +31,11 @@ def root_post():
         return render_template("root.html", text = "Sorry this file extension is not supported", text_t = "Supported filetypes are WAV, AIFF, AIFF-C, FLAC, TXT",score=0)
     else:
         text = microservices.transcribe(file, transcribe_server_url, "sphinx")
-    translated = microservices.translate(text, url=translate_server_url, model_id=100) 
+    model_name = request.form.get("model_type", None)
+    return model_name
+    if model_type not in ("small", "big"):
+        return render_template("root.html", text = "Non valid model type was choosen", text_t="You can choose between small and big")
+    model_id = model_type[model_name]
+    translated = microservices.translate(text, url=translate_server_url, model_id=model_id) 
     return render_template("root.html", text=translated.src, text_t=translated.tgt, score=translated.score)
 
