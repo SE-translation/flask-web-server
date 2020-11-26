329f5777362fc973fb683fe2bca76db70844fbfd
Quinten <tintin10q@hotmail.com>
2020-11-26 23:38:19 +0100

    Removed model choice

--- web_frontend/blueprints/root.py
+++ web_frontend/blueprints/root.py
@@ -17,8 +17,6 @@ def root_get():
     return render_template("root.html")
 
 
-model_type = {"small":100, "big":101}
-
 @root_blueprint.route("/", methods=["POST"])
 def root_post():
     file = request.files['audio_file']
@@ -26,16 +24,11 @@ def root_post():
         return render_template("root.html", text="Something was wrong with the filename")
     extension = os.path.splitext(file.filename)[-1]
     if extension.upper() == ".TXT":
-        text = file.read()
+        text = file.read().decode()
     elif extension.upper() not in (".WAV", ".AIFF",".AIFF-C",".FLAC"):
         return render_template("root.html", text = "Sorry this file extension is not supported", text_t = "Supported filetypes are WAV, AIFF, AIFF-C, FLAC, TXT",score=0)
     else:
         text = microservices.transcribe(file, transcribe_server_url, "sphinx")
-    model_name = request.form.get("model_type", None)
-    return model_name
-    if model_type not in ("small", "big"):
-        return render_template("root.html", text = "Non valid model type was choosen", text_t="You can choose between small and big")
-    model_id = model_type[model_name]
-    translated = microservices.translate(text, url=translate_server_url, model_id=model_id) 
-    return render_template("root.html", text=translated.src, text_t=translated.tgt, score=translated.score)
+    translated = microservices.translate(text, url=translate_server_url, model_id=100)
+    return render_template("root.html", text=text, text_t=translated.tgt, score=translated.score)
 
